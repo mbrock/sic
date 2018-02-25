9aef20a4130bffdfcbc5efe16e3e33dda0f307e1
Mikael Brockman <mikael@brockman.se>
2018-02-25 21:48:18 +0200

    shell.nix

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{ nixpkgs ? import <nixpkgs> {} }:
+let
+  pkgs = import (
+    (import <nixpkgs> {}).fetchFromGitHub {
+      owner = "NixOS";
+      repo = "nixpkgs-channels";
+      rev = "831ef4756e372bfff77332713ae319daa3a42742";
+      sha256 = "1rbfgfp9y2wqn1k0q00363hrb6dc0jbqm7nmnnmi9az3sw55q0rv";
+    }
+  ) {};
+  drv = pkgs.callPackage (import ./default.nix) {};
+in
+  if pkgs.lib.inNixShell then drv.env else drv
