c04045d6dcfe51dcd780b96e329b469f3ce6e285
Mikael Brockman <mikael@brockman.se>
2018-05-02 11:42:48 +0300

    Add a "nix run" based agda script

--- shell.nix
+++ shell.nix
@@ -1,12 +1,1 @@
-{ nixpkgs ? import <nixpkgs> {} }:
-let
-  pkgs = import (
-    (import <nixpkgs> {}).fetchFromGitHub {
-      owner = "NixOS";
-      repo = "nixpkgs-channels";
-      rev = "831ef4756e372bfff77332713ae319daa3a42742";
-      sha256 = "1rbfgfp9y2wqn1k0q00363hrb6dc0jbqm7nmnnmi9az3sw55q0rv";
-    }
-  ) {};
-in
-  pkgs.callPackage (import ./default.nix) {}
+(import ./nixpkgs.nix).callPackage (import ./default.nix) {}
